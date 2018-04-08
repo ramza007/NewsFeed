7c0097859eda1997a88f423c06b00d5335b5ebcf
ramza007 <ramzalvo007@gmail.com>
2018-04-08 22:06:36 +0300

    added few chanes to __init__

--- app/__init__.py
+++ app/__init__.py
@@ -20,7 +20,7 @@ def create_app(config_name):
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
-    # setting coniguration
+    # setting conigurations
     from .requests import configure_request
     configure_request(app)
 
